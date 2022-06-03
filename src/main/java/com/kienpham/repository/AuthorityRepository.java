44adca302beb274e889f69131adfecfc28c4d68f
Kein Pham <kienpham.se@gmail.com>
2022-06-03 08:48:43 +0700

    Initial version of bankingservice generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/kienpham/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.kienpham.repository;
+
+import com.kienpham.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/kienpham/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.kienpham.security;
